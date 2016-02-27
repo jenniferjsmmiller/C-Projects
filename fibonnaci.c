4ca29e7dce3446a6b87f95da28b782ac7633d796
Jenn Miller <jenniferjsmmiller@gmail.com>
2016-02-26 17:14:14 -0800

    cleaning up

--- fibonnaci.c
+++ fibonnaci.c
@@ -1,19 +1,21 @@
 #include <FPT.h>
 
-int main()
+int main() {
+  int num_one, num_two, sum_num, total_sum;
+  num_one = 0;
+  num_two = 1;
+  total_sum = 0;
 
-{
-  int x, y, z, sum ;
-  x=0 ;
-  y=1 ;
-  sum=0 ;
+  //while the second number hasn't hit the limit, 
+  //add num_one and num_two together to get the next fibonnaci number
+  //set num_one to num_two and num_two to the sum of the numbers and add to get next fibonnaci number
   do {
-       z=x+y ;
-       x=y ;
-       y=z ;
-       if (y%2==0)
-         sum+=y ;
-      }while (y<=4000000) ;
-   cout << sum << "\n" ;
+       sum_num = num_one + num_two;
+       num_one = num_two;
+       num_two = sum_num;
+       if (num_two % 2 == 0)
+         total_sum += num_two;
+      } while (num_two <= 4000000);
+   cout << total_sum << "\n";
 }
 
--- palindrome.c
+++ palindrome.c
@@ -1,50 +1,50 @@
 #include <FPT.h>
 
-int palindrome(int n)
+int palindrome(int number) {
+  int reversed = 0;
+  int original = number;
+  if (number < 10) {
+    return 1;
+  }
 
-{
-  int reversed=0 ;
-  int original=n ;
-  if (n < 10){
-    return 1 ;
+  if (number % 10 == 0) {
+    return 0;
   }
-  if (n % 10==0){
-    return 0 ;
+
+  while (number >= 1) {
+    reversed = (reversed * 10) + (number % 10);
+    number /= 10;
   }
-  while (n >= 1){
-    reversed=(reversed*10)+(n%10) ;
-    n=n/10 ;
+
+  if (original == reversed) {
+    return 1;
   }
-  if (original==reversed){
-    return 1 ;
-  }else{
-    return 0 ;
+  else {
+    return 0;
   }
 }
 
-int main()
+int main() {
+  int a, b, c, product;
+  int number = 0;
+  c = 1000;
 
-{
-  int a, b, c, product ;
-  int number=0 ;
-  c=1000 ;
-
-  while (c > 0){
-    a=999 ;
-    while (a > 99){
-      b=999 ;
-      while (b >= a){
-        product=a*b ;
-        if ((product > number)&&(palindrome(product)==1)){
-          number=product ;
+  while (c > 0) {
+    a = 999;
+    while (a > 99) {
+      b = 999;
+      while (b >= a) {
+        product = a * b;
+        if ((product > number) && (palindrome(product) == 1)) {
+          number = product;
         }
-        b=b-1 ;
+        b--;
       }
-      a=a-1 ;
+      a--;
     }
-    c=c-1 ;
+    c--;
   }
-  cout << number << endl ;
+  cout << number << endl;
 }
 
 
